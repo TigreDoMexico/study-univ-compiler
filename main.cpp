b89da39c01926285326786bb4acfeb02bda71be1
David Tigre <tigredavid007@gmail.com>
2018-09-14 13:51:29 -0300

    Divisão de palavras

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@
 // ********************************/
 int main(int argc, char *argv[])
 {
+    //char nome[100];
+
+    //printf("Digite o nome do arquivo:");
+    //scanf("%s", &nome);
+
+    //Tk* tokens = getTokens(nome);
+
     Tk* tokens = getTokens("mdc.c");
 
     return 0;
--- scan.cpp
+++ scan.cpp
@@ -33,18 +33,28 @@ void getNum(FILE *p);
  ***********************************************/
 Tk *getTokens(char *nome)
 {
-    Tk *TkList=NULL, *elemento=NULL;
+    Tk *TkList = NULL, *elemento = NULL;
     FILE *P;
     char c;
 
     P = abreArq(nome);
 
+    linha = 1;
+    ntoken = 0;
+
     do{
+        //printf("linha %d ", linha);
         scan(P);
-        printf("%c", look);
+
+        if(token[0] != '\0')
+            printf("%s\n", token);
+
+        //printf("%c", look);
 
     } while(look != EOF);
 
+    printf("Total de linhas: %d\n", linha);
+    printf("Total de tokens: %d\n", ntoken);
 
 
     return TkList;
@@ -52,26 +62,40 @@ Tk *getTokens(char *nome)
 
 /*********************************
  * Nome: scan
- * desc:
- * returno:
+ * desc: verifica cada caracter de um FILE
+ * returno: null
  ********************************/
 void scan(FILE *p)
 {
     // se look for vazio, skipwhite
     // se
     // se nao verificar se e numero
+
     nextChar(p);
+
+
 
     if(look == ' ' || look == '\t')
         skipWhite(p);
 
+    if(look == '\n'){
+        linha++;
+        nextChar(p);
+    }
 
+
+    if(look >= '0' && look <= '9'){
+        getNum(p);
+    }else{
+        if(isOp(look) == 0)
+            getWord(p);
+    }
 }
 
 /*********************************
  * Nome: abreArq
- * desc:
- * returno:
+ * desc: abre o arquivo .c no formato string e salva em in
+ * returno: FILE in
  ********************************/
 FILE *abreArq(char *nome)
 {
@@ -190,19 +214,28 @@ void skipComment(FILE *p)
  ********************************/
 void getNum(FILE *p)
 {
+
+
     int i;
 
 }
 
 /*********************************
  * Nome: getWord
- * desc:
+ * desc: salva uma palavra na variavel token[256]
  * returno:
  ********************************/
 void getWord(FILE *p)
 {
-    int i;
+    int i = 0;
 
+    memset(token, 0, 256);
+
+    do{
+        token[i] = look;
+        nextChar(p);
+        i++;
+    }while(look != ' ' && look != '\n' && isOp(look) == 0 && i < 256);
 }
 
 /*********************************
@@ -220,10 +253,10 @@ void getOp(FILE *p)
 /*********************************
  * Nome: isOp
  * desc:
- * returno:
+ * returno: 1 - Se é caractere especial / 0 - se não é
  ********************************/
 int isOp(char c)
 {
-    return (strchr("#.+-*/<>:=!", c) != NULL); //Returns a pointer to the first occurrence of character in the C string str.
+    return (strchr("#,+-*/<>:=!", c) != NULL); //Returns a pointer to the first occurrence of character in the C string str.
 }
 
